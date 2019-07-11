bfb706629f729d32ddac71fedf30b8bca93a6abd
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-07-11 16:11:17 +0100

    Conditional Rendering (Better Way)

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -45,16 +45,11 @@ class App extends Component {
       cursor: 'pointer'
     };
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App.</h1>
-        <p>This is really working!</p>
-        <button 
-          style={style}
-          onClick={this.togglePeopleHandler}>Toggle People</button>
-        {
-          this.state.showPeople ? 
-          <div>
+    let people = null;
+
+    if (this.state.showPeople) {
+      people = (
+        <div>
           <Person 
             name={this.state.people[0].name} 
             age={this.state.people[0].age}
@@ -63,8 +58,18 @@ class App extends Component {
             name={this.state.people[1].name} 
             age={this.state.people[1].age}
             changed={this.nameChangedHandler}>Hy Hobbies: Racing</Person>
-          </div> : null
-        }
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App.</h1>
+        <p>This is really working!</p>
+        <button 
+          style={style}
+          onClick={this.togglePeopleHandler}>Toggle People</button>
+        {people}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, 
