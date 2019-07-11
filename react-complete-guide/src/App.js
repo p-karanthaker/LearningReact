110c9fd8e77adc3d2f6051478012d44cbf237c74
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-07-11 13:32:15 +0100

    Revert Back To Class Components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,24 +1,19 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-
-  const [ peopleState, setPeopleState ] = useState({
+class App extends Component {
+  state = {
     people: [
       {name: 'Karan', age: 25},
       {name: 'Max', age: 28}
     ]
-  });
+  }
 
-  const [ otherState, setOtherState ] = useState('some other value');
-
-  console.log(peopleState, otherState);
-
-  const switchNameHandler = () => {
+  switchNameHandler = () => {
     // console.log('Was clicked!');
-    // DONT DO THIS: peopleState.people[0].name = 'Karan Thaker';
-    setPeopleState({
+    // DONT DO THIS: this.state.people[0].name = 'Karan Thaker';
+    this.setState({
       people: [
         {name: 'Karan Thaker', age: 25},
         {name: 'Max', age: 29}
@@ -26,17 +21,19 @@ const app = props => {
     });
   }
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App.</h1>
-      <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={peopleState.people[0].name} age={peopleState.people[0].age} />
-      <Person name={peopleState.people[1].name} age={peopleState.people[1].age}>Hy Hobbies: Racing</Person>
-    </div>
-  );
-  // return React.createElement('div', {className: 'App'}, 
-  //   React.createElement('h1', null, 'Does this work now?'))
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App.</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}>Hy Hobbies: Racing</Person>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, 
+    //   React.createElement('h1', null, 'Does this work now?'))
+  }
 }
 
-export default app;
+export default App;
