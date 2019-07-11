35c0b543606654f1b010e78eda9bc9460ee34c30
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-07-11 16:01:31 +0100

    Conditional Rendering

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -17,7 +17,8 @@ class App extends Component {
       people: [
         {name: newName, age: 25},
         {name: 'Max', age: 29}
-      ]
+      ],
+      showPeople: false
     });
   }
 
@@ -28,6 +29,11 @@ class App extends Component {
         {name: event.target.value, age: 29}
       ]
     });
+  }
+
+  togglePeopleHandler = () => {
+      const showing = this.state.showPeople;
+      this.setState({showPeople: !showing});
   }
 
   render() {
@@ -45,15 +51,20 @@ class App extends Component {
         <p>This is really working!</p>
         <button 
           style={style}
-          onClick={() => this.switchNameHandler('Karan!!')}>Switch Name</button>
-        <Person 
-          name={this.state.people[0].name} 
-          age={this.state.people[0].age}
-          click={this.switchNameHandler.bind(this, 'KARAN THAKER')} />
-        <Person 
-          name={this.state.people[1].name} 
-          age={this.state.people[1].age}
-          changed={this.nameChangedHandler}>Hy Hobbies: Racing</Person>
+          onClick={this.togglePeopleHandler}>Toggle People</button>
+        {
+          this.state.showPeople ? 
+          <div>
+          <Person 
+            name={this.state.people[0].name} 
+            age={this.state.people[0].age}
+            click={this.switchNameHandler.bind(this, 'KARAN THAKER')} />
+          <Person 
+            name={this.state.people[1].name} 
+            age={this.state.people[1].age}
+            changed={this.nameChangedHandler}>Hy Hobbies: Racing</Person>
+          </div> : null
+        }
       </div>
     );
     // return React.createElement('div', {className: 'App'}, 
