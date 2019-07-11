d73352e5bc00dbed0e8dc997f58f3e6ce3f6ee39
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-07-11 13:46:24 +0100

    Passing Method References Between Components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -10,12 +10,12 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DONT DO THIS: this.state.people[0].name = 'Karan Thaker';
     this.setState({
       people: [
-        {name: 'Karan Thaker', age: 25},
+        {name: newName, age: 25},
         {name: 'Max', age: 29}
       ]
     });
@@ -26,9 +26,14 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}>Hy Hobbies: Racing</Person>
+        <button onClick={() => this.switchNameHandler('Karan!!')}>Switch Name</button>
+        <Person 
+          name={this.state.people[0].name} 
+          age={this.state.people[0].age}
+          click={this.switchNameHandler.bind(this, 'KARAN THAKER')} />
+        <Person 
+          name={this.state.people[1].name} 
+          age={this.state.people[1].age}>Hy Hobbies: Racing</Person>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, 
