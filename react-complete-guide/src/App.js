e6e67c34f551315366cb94732ccc1d3a4ec3fff5
p-karanthaker <p-karanthaker@users.noreply.github.com>
2019-07-12 11:47:38 +0100

    Using Readium For Media Queries

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
   state = {
@@ -97,14 +97,16 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App.</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button 
-          style={style}
-          onClick={this.togglePeopleHandler}>Toggle People</button>
-        {people}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App.</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+            style={style}
+            onClick={this.togglePeopleHandler}>Toggle People</button>
+          {people}
+        </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, 
     //   React.createElement('h1', null, 'Does this work now?'))
