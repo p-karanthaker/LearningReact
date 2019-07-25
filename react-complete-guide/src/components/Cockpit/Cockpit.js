6e7b5ec6f91da2a23796ba8c1595ed601e1485db
Karan Thaker <p-karanthaker@users.noreply.github.com>
2019-07-25 13:59:13 +0100

    Using Refs With Hooks

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,28 +1,31 @@
-import React, { useEffect } from 'react';
-import classes from './Cockpit.css';
+import React, { useEffect, useRef } from "react";
+import classes from "./Cockpit.css";
 
-const cockpit = (props) => {
+const cockpit = props => {
+  const toggleBtnRef = useRef(null);
+
   useEffect(() => {
-    console.log('[Cockpit.js] useEffect');
+    console.log("[Cockpit.js] useEffect");
     // Http request...
-    const timer = setTimeout(() => {
-      alert('Saved data to cloud!');
-    }, 1000);
+    /* const timer = setTimeout(() => {
+      alert("Saved data to cloud!");
+    }, 1000); */
+    toggleBtnRef.current.click();
     return () => {
-      clearTimeout(timer);
-      console.log('[Cockpit.js] cleanup work in useEffect');
+      // clearTimeout(timer);
+      console.log("[Cockpit.js] cleanup work in useEffect");
     };
   }, []);
 
   useEffect(() => {
-    console.log('[Cockpit.js] 2nd useEffect');
+    console.log("[Cockpit.js] 2nd useEffect");
     return () => {
-      console.log('[Cockpit.js] cleanup work in 2nd useEffect');
-    }
+      console.log("[Cockpit.js] cleanup work in 2nd useEffect");
+    };
   });
 
   const assignedClasses = [];
-  let btnClass = '';
+  let btnClass = "";
 
   if (props.showPeople) {
     btnClass = classes.Red;
@@ -38,10 +41,10 @@ const cockpit = (props) => {
   return (
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
-      <p className={assignedClasses.join(' ')}>This is really working!</p>
-      <button
-        className={btnClass}
-        onClick={props.clicked}>Toggle People</button>
+      <p className={assignedClasses.join(" ")}>This is really working!</p>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
+        Toggle People
+      </button>
     </div>
   );
 };
