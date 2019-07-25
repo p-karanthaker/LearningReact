f5cfee887d8e48720184e258567818cafb4ac19a
Karan Thaker <p-karanthaker@users.noreply.github.com>
2019-07-25 21:12:17 +0100

    Adding Build Controls

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
   state = {
@@ -18,7 +19,9 @@ class BurgerBuilder extends Component {
         <div>
           <Burger ingredients={this.state.ingredients} />
         </div>
-        <div>Build Controls</div>
+        <div>
+          <BuildControls />
+        </div>
       </Aux>
     );
   }
