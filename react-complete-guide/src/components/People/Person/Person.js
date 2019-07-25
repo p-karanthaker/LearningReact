750f56b323f48c7caf9a972ca97c3473427eb30b
Karan Thaker <p-karanthaker@users.noreply.github.com>
2019-07-25 13:53:58 +0100

    Using Refs

--- react-complete-guide/src/components/People/Person/Person.js
+++ react-complete-guide/src/components/People/Person/Person.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import classes from './Person.css';
-import withClass from '../../../hoc/withClass';
-import PropTypes from 'prop-types';
+import React from "react";
+import classes from "./Person.css";
+import withClass from "../../../hoc/withClass";
+import PropTypes from "prop-types";
 
 class Person extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
-    console.log('[Person.js] rendering...');
+    console.log("[Person.js] rendering...");
     return (
       <React.Fragment>
-        <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-        <p>{this.props.children}</p>
-        <input type='text' onChange={this.props.changed} value={this.props.name} />
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p key="i2">{this.props.children}</p>
+        <input
+          type="text"
+          // ref={inputEl => {this.inputElement = inputEl;}}
+          ref={this.inputElementRef}
+          key="i3"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
       </React.Fragment>
     );
   }
-};
+}
 
 Person.propTypes = {
   click: PropTypes.func,
