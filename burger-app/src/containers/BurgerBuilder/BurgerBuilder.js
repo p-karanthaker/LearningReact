c9e00ad99c5e6678a7c2a3533922078b3d62e4a4
Karan Thaker <p-karanthaker@users.noreply.github.com>
2019-07-27 11:39:46 +0100

    Hiding The Modal

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -76,6 +76,12 @@ class BurgerBuilder extends Component {
     });
   };
 
+  purchaseCancelHandler = () => {
+    this.setState({
+      purchasing: false
+    });
+  };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -86,7 +92,10 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <div>
