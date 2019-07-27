b1ee1f1ee545cd2338746eb69b2be0fce2a10486
Karan Thaker <p-karanthaker@users.noreply.github.com>
2019-07-27 19:29:34 +0100

    Conditional Redirects

--- http-requests/src/containers/Blog/NewPost/NewPost.js
+++ http-requests/src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -7,7 +8,8 @@ class NewPost extends Component {
   state = {
     title: '',
     content: '',
-    author: 'Karan'
+    author: 'Karan',
+    submitted: false
   };
 
   componentDidMount() {
@@ -22,12 +24,18 @@ class NewPost extends Component {
     };
     axios.post('/posts', data).then(response => {
       console.log(response);
+      this.setState({ submitted: true });
     });
   };
 
   render() {
+    let redirect = null;
+    if (this.state.submitted) {
+      redirect = <Redirect to="/posts" />;
+    }
     return (
       <div className="NewPost">
+        {redirect}
         <h1>Add a Post</h1>
         <label>Title</label>
         <input
