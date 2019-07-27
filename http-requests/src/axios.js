ee57656c7cdad53fda6dc0206b0fa405b41674c2
Karan Thaker <p-karanthaker@users.noreply.github.com>
2019-07-27 15:08:02 +0100

    Creating & Using Axios Instances

---
+++ http-requests/src/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://jsonplaceholder.typicode.com'
+});
+
+instance.defaults.headers.common['Authorization'] = 'AUTH_TOKEN_INSTANCE';
+
+// instance.interceptors.request...
+
+export default instance;
